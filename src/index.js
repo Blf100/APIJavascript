9fd8e8a3a307cc4fae801b58a5aa283026f97a2e
Bruno Lopes <brunofabricio14@hotmail.com>
2022-01-16 22:24:41 -0300

    Trabalhando todos os dias

--- src/index.js
+++ src/index.js
@@ -2,57 +2,62 @@
 const { response, request } = require("express");
 const express = require("express");
 
+// Requesting the uuid
+const { v4: uuidv4 } = require("uuid");
+
 // Instantiating the express
 const app = express();
 
 // Middleware for json
 app.use(express.json());
 
+// Array to database
+const database = []
+
 // Route to search: projects
 app.get("/projects", (require, response) => {
   
-  const query = request.query;
-  
-  console.log(query);
-  
-  return response.json([
-    "Projeto 1",
-    "Projeto 2",
-    "Projeto 3"
-  ]);
+  return response.json(database);
 });
 
 // Rot to post: projects
 app.post("/projects", (request, response) => {
   
-  const body = request.body;
+  const { title, owner } = request.body;
 
-  console.log(body);
+  const project = {
+    id: uuidv4(),
+    title,
+    owner
+  };
   
-  return response.json([
-    
-      "Projeto 1",
-      "Projeto 2",
-      "Projeto 3",
-      "Projeto 4"
-    
-  ]);
+  database.push(project);
+  
+  return response.status(201).json(database);
 });
-
 // Route to put: project
 app.put("/projects/:id", (request, response) => {
 
   const { id } = request.params;
+
+  const { title, owner} = request.body;
+
+  const projectIndex = database.findIndex(project => project.id === id);
+
+  if(projectIndex === -1) {
+    return response.status(400).json({error: "Projet not found!"});
+  } 
+     
+  const project = {
+      id, 
+      title,
+       owner
+     };
+
+  database[projectIndex] = project;
   
-  console.log(id)
-  
-  return response.json([
-    
-    "Projeto 1",
-    "Projeto 2",
-    "Projeto 3",
-    "Projeto 5"
-  ])
+
+  return response.status(201).json(project);
 });
 
 // Route to delet: projects
@@ -72,10 +77,4 @@ app.delete("/projects/:id", (request, response) => {
 });
 
 // Port for server access
-app.listen(3333, () => {
-  console.log("Servidor rodando!");
-});
-
-
-
-
+app.listen(3333);
